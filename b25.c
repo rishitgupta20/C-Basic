dfa4f595636bac8766eb613ba9668f01c89fe7de
Rishit Gupta <92114513+rishitgupta20@users.noreply.github.com>
2021-10-25 00:41:07 +0530

    Add files via upload

---
+++ b25.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int x,y,z;
+    printf("enter the number :\n");
+    scanf("%d",&x);
+    z=sqrt(x);
+    printf("%d",z);
+    return 0;
+}
